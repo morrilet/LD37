25a79edecb9fa31809217c55d62731cd6b6ba40c
Ethan Morrill-Ploum <findwaldoFTLOG@gmail.com>
2016-12-10 13:36:39 -0800

    Added Simple Scripts
    Added a barebones player controller and a simple input controller.

---
+++ LD37/Assets/Scripts/InputManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour 
+{
+	public delegate void KeyAction();
+	public static event KeyAction OnUpPressed;
+	public static event KeyAction OnLeftPressed;
+	public static event KeyAction OnRightPressed;
+
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.W) || Input.GetKeyDown (KeyCode.UpArrow)) 
+		{
+			if (OnUpPressed != null) 
+			{
+				OnUpPressed ();
+			}
+		}
+
+		if (Input.GetKeyDown (KeyCode.A) || Input.GetKeyDown (KeyCode.LeftArrow)) 
+		{
+			if (OnLeftPressed != null) 
+			{
+				OnLeftPressed ();
+			}
+		}
+
+		if (Input.GetKeyDown (KeyCode.D) || Input.GetKeyDown (KeyCode.RightArrow)) 
+		{
+			if (OnRightPressed != null) 
+			{
+				OnRightPressed ();
+			}
+		}
+	}
+}
---
+++ LD37/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This handles player movement. This includes acceleration, velocity dampening,
+/// and turning.
+/// </summary>
+public class PlayerController : MonoBehaviour 
+{
+	private Vector2 velocity;
+	[SerializeField]
+	protected float turnSpeed;
+	[SerializeField]
+	protected float accSpeed;
+
+	void Accelerate(float duration, float accAmount)
+	{
+	}
+
+	void ApplyDampening(out Vector2 vel)
+	{
+		vel = Vector2.zero;
+	}
+
+	void Turn(float degrees)
+	{
+	}
+}
