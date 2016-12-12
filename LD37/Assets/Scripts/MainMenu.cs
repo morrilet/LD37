0564cc66c52a03e8b2167c31b9868eeea92b3eb2
Ethan Morrill-Ploum <findwaldoFTLOG@gmail.com>
2016-12-11 21:29:10 -0800

    Added Menus
    Menus are in. The game over menu still needs a little work to be fully
    functional (score tracking) and needs to be used by the game, but it's
    there to set up for tomorrow.

---
+++ LD37/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour 
+{
+	AsyncOperation loader;
+
+	void Start()
+	{
+		loader = SceneManager.LoadSceneAsync ("MainGame");
+		loader.allowSceneActivation = false;
+	}
+
+	public void PlayGame()
+	{
+		loader.allowSceneActivation = true;
+	}
+
+	public void ViewStory()
+	{
+		SceneManager.LoadScene ("StoryMenu");
+	}
+
+	public void Exit()
+	{
+		Application.Quit ();
+	}
+}
---
+++ LD37/Assets/Scripts/StoryMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class StoryMenu : MonoBehaviour 
+{
+	AsyncOperation loader;
+
+	void Start()
+	{
+		loader = SceneManager.LoadSceneAsync ("MainMenu");
+		loader.allowSceneActivation = false;
+	}
+
+	public void Back()
+	{
+		loader.allowSceneActivation = true;
+	}
+}
