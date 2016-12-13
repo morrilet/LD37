13208c6abbb4ebef266769f8eaf5f56852f351f5
Ethan Morrill-Ploum <findwaldoFTLOG@gmail.com>
2016-12-12 16:20:07 -0800

    Added AudioManger.cs
    The audio manager script is in place. Now just need to make the prefab
    in the main scene and get it working in game.

---
+++ LD37/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AudioManager : MonoBehaviour 
+{
+	[SerializeField]
+	protected static AudioSource musicSource, effectSource;
+	[SerializeField]
+	protected static List<AudioClip> effects, music;
+
+	public static void PlayEffect(string effectName)
+	{
+		for(int i = 0; i < effects.Count; i++)
+		{
+			if(effects[i].name == effectName)
+			{
+				effectSource.volume = 1.0f;
+				effectSource.PlayOneShot (effects [i]);
+			}
+		}
+	}
+
+	public static void PlayEffect(string effectName, float volume)
+	{
+		for(int i = 0; i < effects.Count; i++)
+		{
+			if(effects[i].name == effectName)
+			{
+				effectSource.volume = volume;
+				effectSource.PlayOneShot (effects [i]);
+			}
+		}
+	}
+
+	public static void PlayMusic(string musicName)
+	{
+		for(int i = 0; i < music.Count; i++)
+		{
+			if(music[i].name == musicName)
+			{
+				musicSource.Stop ();
+				musicSource.volume = 1.0f;
+				musicSource.loop = true;
+				musicSource.clip = music [i];
+				musicSource.Play ();
+			}
+		}
+	}
+
+	public static void PlayMusic(string musicName, float volume)
+	{
+		for(int i = 0; i < music.Count; i++)
+		{
+			if(music[i].name == musicName)
+			{
+				musicSource.Stop ();
+				musicSource.volume = volume;
+				musicSource.loop = true;
+				musicSource.clip = music [i];
+				musicSource.Play ();
+			}
+		}
+	}
+}
